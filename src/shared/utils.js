5137657255c69ce99a8d22079a1371b84395dd7e
François Vaux <madx@yapok.org>
2015-06-23 13:16:13 +0200

    Add a uniqueId generator

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,11 @@
 export function rand(min, max) {
-	return ~~(Math.random() * (max - min + 1)) + min
+  return ~~(Math.random() * (max - min + 1)) + min
+}
+
+export function uniqueId() {
+  const timestamp = +new Date()
+  const random = rand(1000, 9999)
+  const digest = [timestamp, random].map(i => i.toString(36)).join("")
+
+  return digest
 }
