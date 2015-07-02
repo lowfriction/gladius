3c31b4fa40254f048f8551ab384334a6b0dfef0c
Patrice Blin <dev@patriceblin.com>
2015-07-02 23:51:49 +0200

    first attempt for a game loop server side (not tested/may not compile)

--- src/server/index.js
+++ src/server/index.js
@@ -9,12 +9,57 @@ const app = express()
 const server = new http.Server(app)
 const io = socketio(server)
 
+const game = new Game()
+
 app.use(express.static(path.join(__dirname, "public")))
 
+
 io.on("connection", (socket) => {
+  const player = new Player(),
+        name = socket.handshake.address
+  game.players.set(name, player)
+
   socket.on("disconnect", () => {
+    game.players.delete(name)
+  })
+  socket.on("update", () => {
+    if (game.players.has(name)) {
+      game.players.set(name, player)
+    }
   })
 })
 
+class Game {
+  constructor() {
+    this.framerate = 1/60 * 1000
+    this.frame = null
+    this.renderer = null
+    this.players = new Map()
+  }
+}
+
+Game.newFrame = function()
+{
+  this.frame = setTimeout(this.loop, this.framerate)
+}
+
+Game.onFrame = function(step)
+{
+  io.sockets.emit("update", Array.from(this.players.values()))
+}
+
+Game.loop = function()
+{
+  this.newFrame()
+
+  var now = new Date().getTime(),
+      step = now - this.rendered
+
+  this.rendered = now
+
+  this.onFrame(step)
+}
+
 server.listen(3000)
 
+game.gameLoop()
--- src/shared/player.js
+++ src/shared/player.js
@@ -1,5 +1,7 @@
 export default class Player {
   constructor(opts) {
     this.name = opts.name
+    this.x = rand(10, 100)
+    this.y = rand(10, 100)
   }
 }
