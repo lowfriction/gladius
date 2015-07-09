1820f0005f921a5080cb0ebbd142df69b7581e27
François Vaux <madx@yapok.org>
2015-07-09 09:57:34 +0200

    Extract common Actor behaviour to its own class

---
+++ src/shared/actor.js
@@ -0,0 +1,12 @@
+export default class Actor {
+  constructor(opts = {}) {
+    this.x = opts.x || 0
+    this.y = opts.y || 0
+    this.speed = opts.speed || 1
+  }
+
+  setPosition(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
--- src/shared/player.js
+++ src/shared/player.js
@@ -1,17 +1,10 @@
+import Actor from "./actor"
 import InputState from "./input_state"
 
-export default class Player {
-  constructor(opts) {
-    opts = opts || {}
+export default class Player extends Actor {
+  constructor(opts = {}) {
+    super(opts)
     this.name = opts.name
-    this.x = opts.x || 0
-    this.y = opts.y || 0
     this.inputState = new InputState()
-    this.speed = 1
-  }
-
-  setPosition(x, y) {
-    this.x = x
-    this.y = y
   }
 }
